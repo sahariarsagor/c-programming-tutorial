fcbfca229bcd435463bb6322b6fa579197f12e68
Sahariar Sagor <contactsahariar@gmail.com>
2024-09-15 18:09:28 +0600

    basic syntax

---
+++ lecture_04.c
@@ -0,0 +1,12 @@
+//Basic Syntax of a C Program: 
+// #include<stdio.h>
+// int main(){
+//     return 0;
+// }
+
+//Example of a Hello World Program: 
+#include<stdio.h>
+int main(){
+    printf("Hello World");
+    return 0;
+}
