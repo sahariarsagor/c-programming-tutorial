cde01f2d51c09eeaeaacc63bd85ec48c5311b5f7
Sahariar Sagor <contactsahariar@gmail.com>
2024-09-15 18:04:54 +0600

    output and get started

---
+++ lecture_05.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+
+    //Hi I am comment.
+
+    /*
+        Hello my name 
+        is sahariar sagor. 
+    */ 
+    printf("Hello Sahariar \n"); 
+
+    return 0;
+}
