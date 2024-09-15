c9966545b9bb8bc43b59f29424c241a25a3bd65b
Sahariar Sagor <contactsahariar@gmail.com>
2024-09-15 18:06:05 +0600

    Variable and Data Type

---
+++ lecture_06.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main() {
+
+    //Declaring variable: 
+    // int myAge = 50; 
+    // printf("My age is: %d", myAge);
+
+    // int myNum, myAge, myRoll;
+
+    // myRoll = 205049;
+    // printf("%d", myRoll); 
+
+    int myRoll = 205049;
+    float myPoint = 4.50;
+    double mysomething = 21546.125422;
+    char myChar = 'S';
+
+    printf("%d \n", myRoll);
+    printf("%f \n", myPoint);
+    printf("%lf \n", mysomething);
+    printf("%c \n", myChar);
+
+    return 0;
+}
